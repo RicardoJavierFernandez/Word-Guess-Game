ad8d14b5be9303373a106b03884e57d14b2f0979
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-06-25 21:26:48 -0400

    Javascript logic for dashes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 document.onkeyup = function(event) {
-    console.log(event.key);
+    var keyPress = event.key;
+    document.getElementById('selectedLetter').innerHTML = keyPress;
 }
 
-// Create a array of words to be used in Hangman game
+// Create an array of words to be used in Hangman game
 var wordsToGuess = ["Dolphins", "Hurricanes", "Marlins", "Panthers", "Heat"];
 // Create a variable to store the length of the array to be used in the selectRandomNumber function
 var lengthOfList = wordsToGuess.length;
 
-// Create a function to select a random number between 0 and 4
+// Create a function to select a random number between 0 and the length of wordsToGuess - 1
 function selectRandomNumber(number){
     // Math.random() will return a float number between 0 and 1
     var randomInteger = Math.floor(Math.random() * Math.floor(number))
@@ -17,4 +18,10 @@ function selectRandomNumber(number){
 
 // Store the output of the selectRandomNumber function to be used to select an index in the wordsToGuess array
 var randomIndex = selectRandomNumber(lengthOfList);
-console.log(wordsToGuess[randomIndex])
+
+var selectedWord = wordsToGuess[randomIndex]
+var lengthOfSelectedWord = selectedWord.length
+var numberOfDashes = '_ '.repeat(lengthOfSelectedWord)
+document.getElementById("dashes").innerHTML = numberOfDashes
+
+
